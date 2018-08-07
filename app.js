12d40e8fff036ba6377a8b4892e04ead5fc097ea
Unknown <asel18.development@gmail.com>
2018-08-07 23:20:03 +0200

    Configure Headers CORS

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 // configurar cabeceras http
+app.use((req, res, next) => {
+
+  res.header("Access-Control-Allow-Origen", "*");
+  res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Allow-Request-Method");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+
+  next();
+});
 
 //rutas base
 app.use(API_ROUTE, user_routes);
